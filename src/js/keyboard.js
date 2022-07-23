07c16c550c4b70363b37325d98ffc929796c0a61
jingwangjjang <55347374+jingwangjjang@users.noreply.github.com>
2022-07-23 05:14:46 -0700

    FIRST

---
+++ src/js/keyboard.js
@@ -0,0 +1,105 @@
+export class Keyboard {
+  #switchEl;
+  #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
+  #keyPress = false;
+  #mouseDown = false;
+
+  constructor() {
+    this.#assignElement();
+    this.#addEvent();
+  }
+
+  #assignElement() {
+    this.#containerEl = document.getElementById("container");
+    // this.#containerEl is faster than getElementById
+    this.#switchEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("input");
+  }
+
+  #addEvent() {
+    this.#switchEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+    this.#inputEl.addEventListener("input", this.#onInput);
+    this.#keyboardEl.addEventListener(
+      "mousedown",
+      this.#onMouseDown.bind(this)
+    );
+    // You can also mouseup outside the keyboardEl, so add it in the document
+    document.addEventListener("mouseup", this.#onMouseUp.bind(this));
+  }
+
+  #onMouseUp(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = false;
+
+    const keyEl = event.target.closest("div.key");
+    // !! => Type casting to boolean
+    const isActive = !!keyEl?.classList.contains("active");
+    const val = keyEl?.dataset.val;
+    if (isActive && !!val && val !== "Space" && val !== "Backspace") {
+      this.#inputEl.value += val;
+    }
+    if (isActive && val === "Space") {
+      this.#inputEl.value += " ";
+    }
+    if (isActive && val === "Backspace") {
+      this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+    }
+    this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+  }
+
+  #onMouseDown(event) {
+    if (this.#keyPress) return;
+    this.#mouseDown = true;
+
+    event.target.closest("div.key")?.classList.add("active");
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+
+  #onKeyDown(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = true;
+
+    /* Not working */
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code = ${event.code}]`)
+      ?.classList.add("active"); // Optional chaining
+  }
+
+  #onKeyUp(event) {
+    if (this.#mouseDown) return;
+    this.#keyPress = false;
+
+    this.#keyboardEl
+      .querySelector(`[data-code = ${event.code}]`)
+      ?.classList.remove("active");
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
+  }
+}
